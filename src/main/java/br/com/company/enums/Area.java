26362a6422c81101ffab8d326d94655265a18f62
José Ricardo <56279938+ricardoricarte@users.noreply.github.com>
2022-12-08 10:30:54 -0300

    Update Area.java

--- src/main/java/br/com/company/enums/Area.java
+++ src/main/java/br/com/company/enums/Area.java
@@ -10,7 +10,7 @@ public enum Area {
   BACKEND("Backend"),
   FRONTEND("Frontend"),
   TESTES("Testes"),
-  INFRAEXTRUTURA("Infraextrutura"),
+  INFRAESTRUTURA("Infraestrutura"),
   ESTAGIARIOS("Estagiários"),
   RH("Recursos Humanos");
 
